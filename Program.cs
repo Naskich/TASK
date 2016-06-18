a921323d3c48d30c4a3a070a82759ff2d21805d8
Atanas Krastev <at_krastev@yahoo.com>
2016-06-18 20:37:21 +0300

    C# Coordinate Systems
    Task form exam C# - Input data is being read from the console.
    The number X is on the first input line.
    The number Y is on the second input line.
    The input data will always be valid and in the format described. There
    is no need to check it explicitly.
    Output
    The output data must be printed on the console.
    On the only output line you must print an integer number between 0 and
    6, depending on the location of the given point in the coordinate
    system.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CartesianCoordinateSystem
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("Input numbers for 'x' and 'y' in Coordinate System ");
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int y = int.Parse(Console.ReadLine());
+                if (x == 0 && y == 0)
+            {
+                Console.WriteLine("Output {0}", 0);
+            }
+                 if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Output {0}", 1);
+            }
+                 if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Output {0}", 2);
+            }
+                if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Output {0}", 3);
+            }
+                 if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Output {0}", 4);
+            }
+                if (x == 0 && y > 0)
+            {
+                    Console.WriteLine("Output {0}", 5);
+            }
+                if (x > 0 && y == 0)
+            {
+                    Console.WriteLine("Output {0}", 6);
+            }       
+        }
+    }
+}
